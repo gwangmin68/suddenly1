8db49183907be9ce62344e32738da2a24e2bfbd7
lastmotion4 <lastmotion4@outlook.kr>
2018-06-12 20:47:59 +0900

    디자인 2

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.b_order = new System.Windows.Forms.Button();
             this.b_like = new System.Windows.Forms.Button();
@@ -40,14 +41,26 @@
             this.b_list = new System.Windows.Forms.Button();
             this.Logo = new System.Windows.Forms.Panel();
             this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.Logo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = System.Drawing.Color.RosyBrown;
+            flowLayoutPanel1.Location = new System.Drawing.Point(562, 19);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new System.Drawing.Size(67, 14);
+            flowLayoutPanel1.TabIndex = 4;
+            // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -66,7 +79,7 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.pictureBox2);
             this.panel3.Controls.Add(this.textBox1);
             this.panel3.Controls.Add(this.b_order);
             this.panel3.Controls.Add(this.b_like);
@@ -77,20 +90,22 @@
             this.panel3.Size = new System.Drawing.Size(936, 74);
             this.panel3.TabIndex = 1;
             // 
-            // pictureBox1
+            // pictureBox2
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(505, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(370, 26);
+            this.textBox1.Location = new System.Drawing.Point(372, 28);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(127, 21);
             this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // b_order
             // 
@@ -98,9 +113,11 @@
             this.b_order.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.b_order.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.b_order.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.b_order.Location = new System.Drawing.Point(838, 5);
+            this.b_order.Image = ((System.Drawing.Image)(resources.GetObject("b_order.Image")));
+            this.b_order.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.b_order.Location = new System.Drawing.Point(834, 5);
             this.b_order.Name = "b_order";
-            this.b_order.Size = new System.Drawing.Size(95, 67);
+            this.b_order.Size = new System.Drawing.Size(85, 67);
             this.b_order.TabIndex = 7;
             this.b_order.Text = "Order";
             this.b_order.UseVisualStyleBackColor = true;
@@ -113,9 +130,9 @@
             this.b_like.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.b_like.Image = ((System.Drawing.Image)(resources.GetObject("b_like.Image")));
             this.b_like.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.b_like.Location = new System.Drawing.Point(737, 5);
+            this.b_like.Location = new System.Drawing.Point(743, 5);
             this.b_like.Name = "b_like";
-            this.b_like.Size = new System.Drawing.Size(95, 67);
+            this.b_like.Size = new System.Drawing.Size(85, 67);
             this.b_like.TabIndex = 6;
             this.b_like.Text = "Like";
             this.b_like.UseVisualStyleBackColor = true;
@@ -128,11 +145,11 @@
             this.b_cart.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.b_cart.Image = ((System.Drawing.Image)(resources.GetObject("b_cart.Image")));
             this.b_cart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.b_cart.Location = new System.Drawing.Point(636, 5);
+            this.b_cart.Location = new System.Drawing.Point(652, 5);
             this.b_cart.Name = "b_cart";
-            this.b_cart.Size = new System.Drawing.Size(95, 67);
+            this.b_cart.Size = new System.Drawing.Size(85, 67);
             this.b_cart.TabIndex = 5;
-            this.b_cart.Text = "Cart";
+            this.b_cart.Text = " Cart";
             this.b_cart.UseVisualStyleBackColor = true;
             // 
             // b_list
@@ -141,17 +158,20 @@
             this.b_list.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.b_list.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.b_list.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.b_list.Location = new System.Drawing.Point(535, 5);
+            this.b_list.Image = ((System.Drawing.Image)(resources.GetObject("b_list.Image")));
+            this.b_list.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.b_list.Location = new System.Drawing.Point(561, 5);
             this.b_list.Name = "b_list";
-            this.b_list.Size = new System.Drawing.Size(95, 67);
+            this.b_list.Size = new System.Drawing.Size(85, 67);
             this.b_list.TabIndex = 4;
             this.b_list.Text = "Rank";
             this.b_list.UseVisualStyleBackColor = true;
             // 
             // Logo
             // 
-            this.Logo.BackColor = System.Drawing.SystemColors.Desktop;
-            this.Logo.Location = new System.Drawing.Point(46, 19);
+            this.Logo.BackColor = System.Drawing.Color.RosyBrown;
+            this.Logo.Controls.Add(this.pictureBox1);
+            this.Logo.Location = new System.Drawing.Point(62, 19);
             this.Logo.Name = "Logo";
             this.Logo.Size = new System.Drawing.Size(130, 118);
             this.Logo.TabIndex = 2;
@@ -163,11 +183,21 @@
             this.vScrollBar1.Size = new System.Drawing.Size(17, 494);
             this.vScrollBar1.TabIndex = 3;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(936, 589);
+            this.Controls.Add(flowLayoutPanel1);
             this.Controls.Add(this.vScrollBar1);
             this.Controls.Add(this.Logo);
             this.Controls.Add(this.panel3);
@@ -178,6 +208,8 @@
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Logo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -195,6 +227,7 @@
         private System.Windows.Forms.Panel Logo;
         private System.Windows.Forms.VScrollBar vScrollBar1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
