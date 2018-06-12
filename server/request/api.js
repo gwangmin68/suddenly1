af192df6cd4072b2046ea289b6c326a89654bb12
서상희 <tbvjaos510@naver.com>
2018-06-12 20:08:53 +0900

    과자 추가

--- server/request/api.js
+++ server/request/api.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var utf8 = require('utf8');
-function statusCodeErrorHandler(statusCode, callback , data) {
+
+function statusCodeErrorHandler(statusCode, callback, data) {
     switch (statusCode) {
         case 200:
             callback(null, JSON.parse(data));
@@ -9,24 +10,40 @@ function statusCodeErrorHandler(statusCode, callback , data) {
             callback('error', JSON.parse(data));
             break;
     }
-} 
+}
+/**
+ * 
+ * @param {String} str 
+ */
+
 module.exports = {
-    api_get : 
-    function (method, query, callback){
-        var options={
-            headers:{'Content-Type': 'application/json',
-                    'X-Naver-Client-Id':'6_URPDXcfUAsiCSAJLqb',
-                    'X-Naver-Client-Secret':'kjWpzg2lk2'},
-            url:'https://openapi.naver.com/v1/search/shop.json?query=' +utf8.encode(query) + '&sort=asc',
-            body:null,
-        };
-        
-        console.log(options.url);
-        request.get(options, function(err, res, result){
+    api_get: function (method, query, callback) {
+        var options = {};
+        if (method == 'NAVER') {
+            options = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'X-Naver-Client-Id': '6_URPDXcfUAsiCSAJLqb',
+                    'X-Naver-Client-Secret': 'kjWpzg2lk2'
+                },
+                url: 'https://openapi.naver.com/v1/search/shop.json?query=' + '%ED%8F%AC%EC%B9%B4%EC%B9%A9',
+                body: null,
+
+            };
+        }
+        else if(method == ''){
+
+        }
+        console.log('%ED%8F%AC%EC%B9%B4%EC%B9%A9');
+        console.log(utf8.encode('포카칩').split('\\')[0]);
+        request.get(options, function (err, res, result) {
+            if (err) {
+                console.log(err);
+            }
             statusCodeErrorHandler(res.statusCode, callback, result);
         });
 
     }
 
-    
+
 };
---
+++ server/request/snacks.js
@@ -0,0 +1,11 @@
+module.exports = {
+    '포카칩' : '%ED%8F%AC%EC%B9%B4%EC%B9%A9',
+    '가나' : '%xEA%xB0%x80%xEB%x82%x98',
+    '스윙칩' : '%xEC%x8A%xA4%xEC%x9C%x99%xEC%xB9%xA9',
+    '새우깡' : '%xEC%x83%x88%xEC%x9A%xB0%xEA%xB9%xA1',
+    '꼬북칩' : '%xEA%xBC%xAC%xEB%xB6%x81%xEC%xB9%xA9',
+    '쫄병' : '%xEC%xAB%x84%xEB%xB3%x91',
+    '뿌셔뿌셔' : '%xEB%xBF%x8C%xEC%x85%x94%xEB%xBF%x8C%xEC%x85%x94',
+    '프링글스' : '%xED%x94%x84%xEB%xA7%x81%xEA%xB8%x80%xEC%x8A%xA4'
+
+};
