95903b669dff3e83b03f123cd3bc126022eb9a90
서상희 <tbvjaos510@naver.com>
2018-06-12 20:35:03 +0900

    server 수정

--- server/request/api.js
+++ server/request/api.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var utf8 = require('utf8');
-
+var snacks = require('./snacks');
 function statusCodeErrorHandler(statusCode, callback, data) {
     switch (statusCode) {
         case 200:
@@ -11,10 +11,12 @@ function statusCodeErrorHandler(statusCode, callback, data) {
             break;
     }
 }
-/**
- * 
- * @param {String} str 
- */
+function NAVER_find_snack(snack, count){
+    if (snacks[snack] == null){
+        return null;
+    }
+    return 'https://openapi.naver.com/v1/search/shop.json?query=' + snacks[snack] + '&display=' + count;
+}
 
 module.exports = {
     api_get: function (method, query, callback) {
@@ -26,7 +28,7 @@ module.exports = {
                     'X-Naver-Client-Id': '6_URPDXcfUAsiCSAJLqb',
                     'X-Naver-Client-Secret': 'kjWpzg2lk2'
                 },
-                url: 'https://openapi.naver.com/v1/search/shop.json?query=' + '%ED%8F%AC%EC%B9%B4%EC%B9%A9',
+                url: NAVER_find_snack('포카칩', 10),
                 body: null,
 
             };
@@ -34,8 +36,9 @@ module.exports = {
         else if(method == ''){
 
         }
-        console.log('%ED%8F%AC%EC%B9%B4%EC%B9%A9');
-        console.log(utf8.encode('포카칩').split('\\')[0]);
+        if (options.url == null){
+            callback(err, 'snack not exiets');
+        }
         request.get(options, function (err, res, result) {
             if (err) {
                 console.log(err);
